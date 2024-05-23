893bbd825d08920b1a26daa81d54e6f46ee723f7
DennyAhn <aa5051140@gmail.com>
2024-05-23 16:29:53 +0900

    Initial commit

---
+++ app/src/main/java/com/flius/guidy/ReviewActivity2.kt
@@ -0,0 +1,17 @@
+package com.flius.guidy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ReviewActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review2)
+
+
+
+    }
+}
